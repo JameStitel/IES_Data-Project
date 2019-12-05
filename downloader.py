8b60dee252775625fe8c0a200149526fc487d9d6
Jakub Vosmansky <jakub.vosmansky@timeisltd.com>
2019-12-05 19:37:28 +0100

    ADD: download all stations and save into JSON

--- downloader.py
+++ downloader.py
@@ -3,15 +3,57 @@ import pandas as pd
 import time
 import json
 
-with open('golemio_api_key.json') as f:
-    GOLEMIO_API_KEY = json.load(f)['X-Access-Token']
+
+class GolemioApi:
+    def __init__(self, api_key_path: str):
+        self.api_key = self._load_api_key(api_key_path)
+        self.headers = {'X-Access-Token': self.api_key}
+        self.limit_per_page = 1000
+        self.base_uri = 'https://api.golemio.cz/v1/'
+
+    @staticmethod
+    def _load_api_key(api_key_path: str) -> str:
+        with open(api_key_path) as f:
+            api_key = json.load(f)['X-Access-Token']
+        return api_key
+
+    def _download_page(self, endpoint: str, offset: int, debug: bool = False, **kwargs):
+        parameters = ''
+        for arg, value in kwargs.items():
+            parameters += f'&{arg}={value}'
+        uri = f'{self.base_uri}{endpoint}?limit={self.limit_per_page}&offset={offset}{parameters}'
+        response = requests.get(uri, headers=self.headers)
+        if debug:
+            print(f'code: {response.status_code}, text: {response.text}')
+        if str(response.status_code)[0] != '2':
+            raise ConnectionError(f'Request failed with status code: {response.status_code}')
+        return response.json()
+
+    def _download_all_pages(self, endpoint: str, features: bool):
+        n = 0
+        while True:
+            json_response = self._download_page(endpoint, offset=n)
+            json_response = json_response['features'] if features else json_response
+            n += len(json_response)
+            if len(json_response) == 0:
+                break
+            yield json_response
+
+    def download_all_stations(self):
+        endpoint = 'gtfs/stops'
+        json_responses = self._download_all_pages(endpoint, features=True)
+        all_stations = []
+        for response in json_responses:
+            all_stations.extend(response)
+        with open('data/all_stations.json', 'w') as f:
+            json.dump(all_stations, f, ensure_ascii=True, indent=4)
 
 
+
+
+
+"""
 def main():
-    headers = {
-        'X-Access-Token': GOLEMIO_API_KEY,
-    }
-
     n = 0
     while True:
         # print(n)
@@ -32,8 +74,10 @@ def main():
         # break
 
     # U363Z1P - Malvazinky
-
+"""
 
 if __name__ == '__main__':
-    main()
+    my_api_key_path = 'golemio_api_key.json'
+    golemio = GolemioApi(my_api_key_path)
+    golemio.download_all_stations()
 
