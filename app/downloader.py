baa5f989766774b548ca82d97f7a0d962496382d
Jakub Vosmansky <jakub.vosmansky@timeisltd.com>
2019-12-25 23:40:36 +0100

    UPDATE: refactor, docs

--- app/downloader.py
+++ app/downloader.py
@@ -26,7 +26,16 @@ class GolemioApiDownloader:
             api_key = json.load(f)['X-Access-Token']
         return api_key
 
-    def _download_page(self, endpoint: str, offset: int, debug: bool = False, **kwargs):
+    def _download_page(self, endpoint: str, offset: int, debug: bool = False, **kwargs) -> dict:
+        """
+        Download one page of data for the selected endpoint of Golemio API with the selected parameters as arguments.
+        Page size set by attribute `limit_per_page`, by default to 1000.
+        :param endpoint: the selected endpoint to download data from
+        :param offset: skip this number of first items (sth. like paging)
+        :param debug: set to True if you need to see responses in the console
+        :param kwargs: selected parameters for querying the endpoint
+        :return: dict with data from response
+        """
         parameters = ''
         for arg, value in kwargs.items():
             parameters += f'{arg}={value}&'
@@ -39,6 +48,15 @@ class GolemioApiDownloader:
         return response.json()
 
     def _download_all_pages(self, endpoint: str, features: bool, debug: bool = False, **kwargs) -> Generator:
+        """
+        Download all pages of data for the selected endpoint of Golemio API with the selected parameters as arguments.
+        Page size set by attribute `limit_per_page`, by default to 1000.
+        :param endpoint: the selected endpoint to download data from
+        :param features: bool whether the data coming in response from Golemio API is under features key
+        :param debug: set to True if you need to see responses in the console
+        :param kwargs: selected parameters for querying the endpoint
+        :return: Generator yielding the data for individual pages
+        """
         n = 0
         while True:
             json_response = self._download_page(endpoint, offset=n, debug=debug, **kwargs)
@@ -54,6 +72,10 @@ class GolemioApiDownloader:
             json.dump(data, output_f, ensure_ascii=False, indent=4)
 
     def download_all_stations(self):
+        """
+        Step 1 of the GolemioApiDonwloader. Download all available public transport stops from the Golemio API,
+        and save with all information into json file named `all_stations`.
+        """
         endpoint = 'gtfs/stops'
         json_responses = self._download_all_pages(endpoint, features=True)
         all_stations = []
@@ -64,6 +86,16 @@ class GolemioApiDownloader:
     @staticmethod
     def _save_parents_into_ids_dict(all_ids: dict, children: dict, parent_station_id: str, station_id: str,
                                     station_location: dict, station_name: str) -> Tuple[dict, dict]:
+        """
+        Save only the parent stations into a dict containing just the required information.
+        :param all_ids: dict containing the already saved parent stations
+        :param children: dict containing the already saved child stations
+        :param parent_station_id: id of the parent station if this is a child station
+        :param station_id: id of this station
+        :param station_location: location of this station
+        :param station_name: name of this station
+        :return: dict containing the parent stations as all_ids and dict containing the child stations
+        """
         if parent_station_id:  # ~ if is a child station
             if parent_station_id in children:  # ~ if this child station`s parent is already saved in children
                 if station_id not in children[parent_station_id]['children']:  # prevent duplication of children
@@ -82,6 +114,12 @@ class GolemioApiDownloader:
 
     @staticmethod
     def _save_children_into_ids_dict(all_ids: dict, children: dict) -> dict:
+        """
+        Extend the dict of parent stations (all_ids) by all their children and children of children.
+        :param all_ids: the parent stations dict
+        :param children: the child stations dict
+        :return: parent station dict complemented by all the child stations
+        """
         children_of_children = {}
         child_parent = {}
         for parent_station_id, children in children.items():
@@ -99,6 +137,10 @@ class GolemioApiDownloader:
         return all_ids
 
     def filter_station_ids_enriched(self):
+        """
+        Step 2 of the GolemioApiDonwloader. Transform information about all stations into a json named
+        `all_stations_ids` containing just the required information for parent stations with list of child stations.
+        """
         with open(self.all_stations_path) as input_f:
             all_stations = json.load(input_f)
         all_ids = {}
@@ -116,19 +158,15 @@ class GolemioApiDownloader:
         all_ids = self._save_children_into_ids_dict(all_ids, children)
         self._save_into_json(all_ids, self.all_stations_ids_path)
 
-    def _get_stop_count_for_station_per_day(self, station_id: str, date: str) -> int:
-        endpoint = f'gtfs/stoptimes/{station_id}'
-        kwargs = {
-            'date': date
-        }
-        json_responses = self._download_all_pages(endpoint, features=False, debug=False, **kwargs)
-        stop_count = 0
-        for response in json_responses:
-            stop_count += len(response)
-        return stop_count
-
-    def _build_list_of_uris_for_count_stop(self, all_ids: dict, date: str) -> list:
-        uris = []
+    def _build_list_of_urls_for_count_stop(self, all_ids: dict, date: str) -> list:
+        """
+        Build list of urls for async requests to retrieve number of stops by all public transportation for all the
+        stations and the selected date from Golemio API.
+        :param all_ids: dict of all the stations in parent-children format
+        :param date: the selected date
+        :return: list of urls
+        """
+        urls = []
         offset = 0
 
         kwargs = {
@@ -142,16 +180,23 @@ class GolemioApiDownloader:
             station_id = station
             endpoint = f'gtfs/stoptimes/{station_id}'
             uri = f'{self.base_uri}{endpoint}?{parameters}limit={self.limit_per_page}&offset={offset}'
-            uris.append(uri)
+            urls.append(uri)
             for child_station in properties['children']:
                 station_id = child_station
                 endpoint = f'gtfs/stoptimes/{station_id}'
                 uri = f'{self.base_uri}{endpoint}?{parameters}limit={self.limit_per_page}&offset={offset}'
-                uris.append(uri)
-        return uris
+                urls.append(uri)
+        return urls
 
-    def _build_list_of_uris_for_count_stop_cont(self, date: str) -> list:
-        uris = []
+    def _build_list_of_urls_for_count_stop_cont(self, date: str) -> list:
+        """
+        Build list of urls for async requests to retrieve number of stops by all public transportation for all the
+        stations and the selected date from Golemio API.
+        THIS BUILDS URIS LIST FROM THE REMAINING requests after the last async requesting of the data!
+        :param date: the selected date
+        :return: list of urls
+        """
+        urls = []
         kwargs = {
             'date': date
         }
@@ -164,14 +209,25 @@ class GolemioApiDownloader:
             offset = station['offset']
             endpoint = f'gtfs/stoptimes/{station_id}'
             uri = f'{self.base_uri}{endpoint}?{parameters}limit={self.limit_per_page}&offset={offset}'
-            uris.append(uri)
-        return uris
+            urls.append(uri)
+        return urls
 
     @staticmethod
-    def __exception(request, exception):
+    def __exception(request: requests.models.Response, exception):
+        """
+        Exception handling of each individual request.
+        :param request: the request
+        :param exception: the exception of the request
+        """
         print("Problem: {}: {}".format(request.url, exception))
 
-    def __callback(self, res, **kwargs):
+    def __callback(self, res: requests.models.Response, **kwargs):
+        """
+        Callback for each of the individual requests. Add the number of stops for the individual station into
+        `self.__counted_stops` dict and append data for requesting the next page of stops into the `__remaining_async`.
+        :param res: the response
+        :param kwargs: the kwargs
+        """
         responses = res.json()
         n = len(responses)
         if n and self.__counted_stops.get(responses[0]['stop_id']):
@@ -191,7 +247,11 @@ class GolemioApiDownloader:
                 }
             )
 
-    def __async_requests(self, urls):
+    def __async_requests(self, urls: list):
+        """
+        Async request the urls, handling responses with `__callback` and `__exception`.
+        :param urls: list of the urls to request
+        """
         self.__remaining_async = []
         req = (grequests.get(u, headers=self.headers, hooks=dict(response=self.__callback)) for u in urls)
         grequests.map(req, exception_handler=self.__exception, size=5)
@@ -203,21 +263,26 @@ class GolemioApiDownloader:
             yield {k: d[k] for k in islice(it, n)}
 
     def count_stop_times_per_day(self, date: str):
+        """
+        Step 3 of the GolemioApiDonwloader. Download all stop counts for all the stations from `all_stations_ids.json`
+        and save into json files by the selected date, `all_stop_count_{date}_{page_number}.
+        :param date: the selected date
+        """
         with open(self.all_stations_ids_path) as input_f:
             all_ids = json.load(input_f)
         n = 1
         for chunk in self._split_dict_into_n_sized_chunks(all_ids, 4000):
-            urls = self._build_list_of_uris_for_count_stop(chunk, date)
+            urls = self._build_list_of_urls_for_count_stop(chunk, date)
             self.__async_requests(urls)
             while self.__remaining_async:
-                urls = self._build_list_of_uris_for_count_stop_cont(date)
+                urls = self._build_list_of_urls_for_count_stop_cont(date)
                 self.__async_requests(urls)
             self._save_into_json(self.__counted_stops, f'{self.all_stop_count_path}_{date}_{n}.json')
 
             n += 1
             self.__counted_stops = {}
             print('Sleeping...')
-            time.sleep(30)
+            time.sleep(30)  # prevent possible timeout
             print('Sleeping done...')
 
     @staticmethod
@@ -226,6 +291,11 @@ class GolemioApiDownloader:
 
     @staticmethod
     def _get_child_parent_dict(all_ids: dict) -> dict:
+        """
+        Get dict of child-parent relations in the format: {child1: parent1, child2: parent1, child3: parent2, ...}.
+        :param all_ids: all parent stations with their children
+        :return: dict in the mentioned format
+        """
         child_parent = {}
         for parent_station, properties in all_ids.items():
             for child_station in properties['children']:
@@ -233,6 +303,13 @@ class GolemioApiDownloader:
         return child_parent
 
     def _aggregate_stop_count_per_file(self, stops: dict, all_ids: dict, all_stops: dict) -> dict:
+        """
+        Aggregate the stop count for the selected file.
+        :param stops: stop count from the selected file
+        :param all_ids: all parent stations with their children as in the `all_stations_ids.json`
+        :param all_stops: CURRENT all parent stations dict with the corresponding stop count
+        :return: UPDATED all parent stations dict with the corresponding stop count
+        """
         child_parent = self._get_child_parent_dict(all_ids)
         for station, count in stops.items():
             station = child_parent[station] if station not in all_ids else station
@@ -243,6 +320,12 @@ class GolemioApiDownloader:
         return all_stops
 
     def aggregate_stop_count(self, date: str) -> Tuple[dict, dict]:
+        """
+        Aggregate all stop count (including that of child stations) for all the parent stations for the selected date.
+        :param date: the selected date
+        :return: dict of all the parent stations and the corresponding aggregated stop count; and dict of all parent
+        stations with their children as in the `all_stations_ids.json`
+        """
         with open(self.all_stations_ids_path) as input_f:
             all_ids = json.load(input_f)
         all_stops = {}
@@ -253,8 +336,18 @@ class GolemioApiDownloader:
                     all_stops = self._aggregate_stop_count_per_file(stops, all_ids, all_stops)
         return all_stops, all_ids
 
-    def _assign_stop_count_per_file(self, all_ids: dict, all_stops: dict, parent_stations_with_count: dict, date: str,
-                                    initial: bool) -> dict:
+    def _assign_stop_count(self, all_ids: dict, all_stops: dict, parent_stations_with_count: dict, date: str,
+                           initial: bool) -> dict:
+        """
+        Create dict of parent stations with the necessary information if initial, otherwise take the already loaded
+        data and enriched by aggregated stop count for the selected date
+        :param all_ids: all parent stations with their children as in the `all_stations_ids.json`
+        :param all_stops: all parent stations dict with the corresponding stop count
+        :param parent_stations_with_count: the dict containing already assigned stop counts or empty dict
+        :param date: the selected date
+        :param initial: bool whether there are already any data for stop counts
+        :return: dict of updated (or newly created if `initial=False`) assigned stop counts
+        """
         for station, properties in all_ids.items():
             if initial:
                 __temp_properties = self._copy_dict_without_keys(all_ids[station], ['children'])
@@ -265,13 +358,20 @@ class GolemioApiDownloader:
         return parent_stations_with_count
 
     def assign_stop_count(self, date: str, initial: bool):
+        """
+        Step 3 of the GolemioApiDonwloader. Assign the aggregated already downloaded all stop counts for the selected
+        dateto all the parent stations from `all_stations_ids.json`,
+         and save into json file named `final-stations_with_count`.
+        :param date: the selected date
+        :param initial: bool whether there are already any data for stop counts or this is the initial assignment
+        """
         if initial:
             parent_ids_count = {}
         else:
             with open(self.parent_ids_with_count_path) as f:
                 parent_ids_count = json.load(f)
         all_stops, all_ids = self.aggregate_stop_count(date)
-        parent_ids_count = self._assign_stop_count_per_file(all_ids, all_stops, parent_ids_count, date, initial)
+        parent_ids_count = self._assign_stop_count(all_ids, all_stops, parent_ids_count, date, initial)
         self._save_into_json(parent_ids_count, self.parent_ids_with_count_path)
 
 
--- app/visualizer.py
+++ app/visualizer.py
@@ -13,6 +13,13 @@ class Visualizer:
 
     @staticmethod
     def reformat_data(json_data: dict, date: str) -> list:
+        """
+        Restructure data to a suitable format for conversion into Pandas dataframe and consequent visualization
+        using Plotly.
+        :param json_data: json data downloaded using GolemioApiDownloader
+        :param date: selected date for visualization that is present in the json_data
+        :return: list suitable for conversion into the Pandas dataframe
+        """
         new_json_data = [{
             'id': station_id,
             'name': data['name'],
@@ -23,21 +30,35 @@ class Visualizer:
         return new_json_data
 
     def get_possible_dates(self) -> list:
+        """
+        Get dates for which there are present stop counts in the json_data downloaded with the GolemioApiDownloader.
+        :return: list of the dates
+        """
         return list(next(iter(self.load_data().values()))['count'].keys())
 
-    def plot(self, date, zoom: int = 7):
+    def plot(self, date: str, zoom: int = 7):
+        """
+        Plot the stop counts for the selected date using Plotly Density Mapbox.
+        :param date: the selected date from possible dates
+        :param zoom: non-required argument for zooming the default location of the map, 6 by default, use 7 for jupyter
+        """
         json_data = self.reformat_data(self.load_data(), date)
         df = pd.DataFrame(json_data)
         max_stop_count = df['stop_count'].max()
 
-        fig = px.density_mapbox(df, lat='latitude', lon='longitude', z='stop_count', hover_name='name', radius=15,
-                                color_continuous_scale='inferno', color_continuous_midpoint=max_stop_count / 2.4,
-                                center=dict(lat=49.80, lon=15.20), zoom=zoom, mapbox_style="open-street-map")
+        fig = px.density_mapbox(
+            data_frame=df, lat='latitude', lon='longitude', z='stop_count', hover_name='name',
+            radius=15,  # sets the thickness of each data point in the map
+            color_continuous_midpoint=max_stop_count / 2.4,  # value found by testing to provide good visibility of
+            # points with low number of stop counts per day
+            color_continuous_scale='inferno', mapbox_style="open-street-map",
+            center=dict(lat=49.80, lon=15.20), zoom=zoom,  # center the map on the Czech Republic
+        )
         fig.show()
 
 
 if __name__ == '__main__':
     visualizer = Visualizer()
     # print(visualizer.get_possible_dates())
-    # visualizer.plot('2020-01-02')
+    visualizer.plot('2020-01-02')
     # visualizer.plot('2019-12-07')
